fff834d487a470c7ef154cc1691918b5682607ca
Aabhushan Gautam <carefreeav09@gmail.com>
2023-01-10 20:55:33 +0545

    optimized solution for Palindrome number

--- NonBlindProblem/PalindromeNumber.js
+++ NonBlindProblem/PalindromeNumber.js
@@ -3,17 +3,10 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-  let string = Array.from(x.toString());
+  let string = x.toString();
   let length = string.length - 1;
-  let reverse = [];
   for (let i = length; i >= 0; i--) {
-    reverse[length - i] = string[i];
-  }
-  for (let i = 0; i < reverse.length; i++) {
-    if (reverse[i] != string[i]) {
-      return false;
-    }
-    continue;
+    if (string[length - i] != string[i]) return false;
   }
 
   return true;
