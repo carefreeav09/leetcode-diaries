09f06e3c50acf9a993589bdeb9fff900d148de8f
Aabhushan Gautam <carefreeav09@gmail.com>
2023-01-11 12:50:41 +0545

    Roman Numerals ( Easy )

---
+++ NonBlindProblem/RomanNumerals.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  let romanObjects = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900,
+  };
+
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    let value = s[i];
+    let combinedValue = s[i] + s[i + 1];
+    if (romanObjects[combinedValue]) {
+      sum += romanObjects[combinedValue];
+      i++;
+    } else {
+      sum += romanObjects[value];
+    }
+  }
+
+  return sum;
+};
