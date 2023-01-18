9d45bac4b76d4f641f72cb8a0396a000619cad07
Aabhushan Gautam <carefreeav09@gmail.com>
2023-01-18 23:33:10 +0545

    empty days updated; keep the streak

--- EmptyDays.js
+++ EmptyDays.js
@@ -1,3 +1,4 @@
 // Empty Days are the days where i cant practice leet code because of personal problems. Ill revise them or update this document if i dont practice.
 
 // 1/7/2023 - Saturday - Work on Heartstring.
+// 1/18/2023 - Tuesday - Work on Heartstring.
