fe68fbe6c7663b15a53915503d3224d1d5cf7f52
Aabhushan Gautam <carefreeav09@gmail.com>
2023-01-21 12:41:57 +0545

    add binary: cheating solution

---
+++ NonBlindProblem/AddBinary.js
@@ -0,0 +1,1 @@
+var addBinary = (a, b) => (BigInt(`0B${a}`) + BigInt(`0B${b}`)).toString(2);
--- Practice.js
+++ Practice.js
@@ -1,46 +1,6 @@
-const plusOne = function (nums) {
-  let length = nums.length - 1;
-  if (length == 0) {
-    return String(nums[0] + 1).split("");
-  }
-  let carryOver = 0;
-  for (let i = length; i >= 0; i--) {
-    if (i === length) {
-      if (nums[i] + 1 === 10) {
-        carryOver = 1;
-        nums[i] = 0;
-      } else {
-        nums[i] = nums[i] + 1;
-      }
-    } else if (carryOver === 1) {
-      if (i == 0) {
-        if (nums[i] + 1 === 10) {
-          nums[i] = 0;
-          nums.unshift(1);
-        } else {
-          nums[i] = nums[i] + 1;
-        }
-      } else {
-        if (nums[i] + 1 === 10) {
-          carryOver = 1;
-          nums[i] = 0;
-        } else {
-          nums[i] = nums[i] + 1;
-          carryOver = 0;
-        }
-      }
-    } else {
-      break;
-    }
-  }
+//Two sum -> passed
+// Palindrome number -> passed.
 
-  return nums;
-};
+const palindromeNumber = (n) => {};
 
-const plusOneShort = function (nums) {
-  let number = BigInt(nums.join(""));
-  let result = String(number + 1n);
-  return result.split("");
-};
-
-console.log(plusOneShort([8, 9, 9, 9]));
+console.log(palindromeNumber(-121));
